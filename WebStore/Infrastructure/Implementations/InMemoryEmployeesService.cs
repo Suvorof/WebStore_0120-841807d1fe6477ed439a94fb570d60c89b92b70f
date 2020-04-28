421acccc4145e448419f237a332b7d1310a0ddaf
Konstantin Suvorov <konsu@inbox.ru>
2020-04-28 16:23:00 +0600

    Add project files.

---
+++ WebStore/Infrastructure/Implementations/InMemoryEmployeesService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Реализация интерфейса, хранит информацию в памяти
+    /// </summary>
+    public class InMemoryEmployeesService : IEmployeesService
+    {
+        private readonly List<EmployeeView> _employees;
+
+        public InMemoryEmployeesService()
+        {
+            _employees = new List<EmployeeView>
+            {
+                new EmployeeView
+                {
+                    Id = 1,
+                    FirstName = "Иван",
+                    SurName = "Иванов",
+                    Patronymic = "Иванович",
+                    Age = 22
+                },
+                new EmployeeView
+                {
+                    Id = 2,
+                    FirstName = "Владислав",
+                    SurName = "Петров",
+                    Patronymic = "Иванович",
+                    Age = 35
+                }
+            };
+        }
+
+        public IEnumerable<EmployeeView> GetAll()
+        {
+            return _employees;
+        }
+
+        public EmployeeView GetById(int id)
+        {
+            return _employees.FirstOrDefault(e => e.Id.Equals(id));
+        }
+
+        public void Commit()
+        {
+            // ничего не делаем
+        }
+
+        public void AddNew(EmployeeView model)
+        {
+            model.Id = _employees.Max(e => e.Id) + 1;
+            _employees.Add(model);
+        }
+
+        public void Delete(int id)
+        {
+            var employee = GetById(id);
+            if (employee != null)
+            {
+                _employees.Remove(employee);
+            }
+        }
+    }
+}
